5f554d5efcf8a476bbde10418290b76b76c0d99e
P.Chakresh <chakreshpatha@gmail.com>
2026-03-26 11:25:41 +0530

    fix: force IPv4 for nodemailer to fix Render ENETUNREACH

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,8 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+// Force IPv4 because Render's free tier has issues reaching some SMTP servers over IPv6
+dns.setDefaultResultOrder('ipv4first');
 
 const sendEmail = async (options) => {
   // Create a transporter using ethereal email or a real service
